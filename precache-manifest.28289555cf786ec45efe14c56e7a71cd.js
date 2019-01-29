6331fd2c542090d752c222af6ac3cb13016ac5b4
jkga <johnkennethgibasabella@gmail.com>
2019-01-29 08:47:05 +0800

    Updates

---
+++ precache-manifest.28289555cf786ec45efe14c56e7a71cd.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = [
+  {
+    "url": "91248e395fff7f5262dd.module.js"
+  },
+  {
+    "url": "9116e49bea0d50e03846.module.js"
+  },
+  {
+    "url": "8c62f298505d23aa7889.module.js"
+  },
+  {
+    "url": "88fce1185443d35bf180.module.js"
+  },
+  {
+    "url": "6a7d7ddbb32175bc5fba.module.js"
+  },
+  {
+    "url": "1072b118dbaee837f6b0.module.js"
+  },
+  {
+    "revision": "3b2bf9e30b334e559018",
+    "url": "./mixins/scanner.js"
+  },
+  {
+    "revision": "30807b9799fbd9fdf06b",
+    "url": "./mixins/barcode.js"
+  },
+  {
+    "revision": "e8d8866c7bddbb1084a5",
+    "url": "./index.js"
+  },
+  {
+    "revision": "813cac2af25884ef9b20",
+    "url": "./components/dropdown-loader/index.js"
+  },
+  {
+    "revision": "621cab6d1918d7a59f83",
+    "url": "./components/button-groups/index.js"
+  }
+];
---
+++ precache-manifest.da72532b6dddb982cbf83080f374dd2c.js
@@ -0,0 +1,68 @@
+self.__precacheManifest = [
+  {
+    "url": "1072b118dbaee837f6b0.module.js"
+  },
+  {
+    "revision": "24885bc58c7beb4d183f24e9cdfdf5c7",
+    "url": "index.css"
+  },
+  {
+    "revision": "ddcb4378d69cbf5785791ff0e4e93c8d",
+    "url": "index.html"
+  },
+  {
+    "url": "9116e49bea0d50e03846.module.js"
+  },
+  {
+    "revision": "30807b9799fbd9fdf06b",
+    "url": "./mixins/barcode.js"
+  },
+  {
+    "url": "91248e395fff7f5262dd.module.js"
+  },
+  {
+    "url": "8fab29237a275d13f543.module.js"
+  },
+  {
+    "url": "88fce1185443d35bf180.module.js"
+  },
+  {
+    "url": "8c62f298505d23aa7889.module.js"
+  },
+  {
+    "revision": "356886e85be28b2b69de989c5d20f69e",
+    "url": "assets/img/pwa-512x512.png"
+  },
+  {
+    "revision": "3b2bf9e30b334e559018",
+    "url": "./mixins/scanner.js"
+  },
+  {
+    "revision": "0d8ae2adb3afc399b4da9b9bea74623e",
+    "url": "assets/img/instasfams-logo.png"
+  },
+  {
+    "revision": "24d43d2d494d6db1616bb29cebb24325",
+    "url": "assets/img/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "2a7258e74b5a437359ad3765c79ba5c2",
+    "url": "assets/img/pwa-192x192.png"
+  },
+  {
+    "revision": "87073e010efa05fa0d62e2ede2cbcbaa",
+    "url": "assets/img/fav.ico"
+  },
+  {
+    "revision": "ed5154a1c28e9ef72c32",
+    "url": "./index.js"
+  },
+  {
+    "revision": "813cac2af25884ef9b20",
+    "url": "./components/dropdown-loader/index.js"
+  },
+  {
+    "revision": "05eb6b1527fbcd4e0d02",
+    "url": "./components/button-groups/index.js"
+  }
+];
